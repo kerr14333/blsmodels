ab5716eef4f6ac476ba4956a78ce0fdb012a9396
Christopher Grieves <grievesc@osmr5.otsplab.psb.bls.gov>
2022-01-26 15:39:56 -0500

    Added Fey-Herriot models

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ This repo consists of simulated data that we wish to apply our stan  models to. 
 * **eight_schools** - Eight Schools example centered/non-centered parameterization. I include it because the centered parameterization had what looks like similar behavior to the samgen3 model. Maybe there is a connection? Maybe I am missing something? 
 * **jolts** - JOLTS (Job Openings and Labor Turnover Survey - The survey you have been seeing in the news about the large number of people quitting their jobs) - Programs and simulated data in directory. 
 * **reportlevel** - Our MRP model for Current Employment Statistics (CES) Survey Small Area Modelling using report level (microdata or sample unit) data. Contains programs and simulated data.
-* **samgen3_toysim** - BLS CES samgen3 (Small Area Model Generation 3) model with simulated data.
+* **samgen3_toysim** - BLS CES samgen3 (Small Area Model Generation 3) model with simulated data. I also include the basic Fay Herriot model with `fh_st.stan` and the Fey-Herriot with clustering model `fhs_st.stan`.
 
 Note: This is a personal repo and does not reflect the opinions of the Bureau of Labor Statistics (BLS)
---
+++ samgen3_toysim/step1_fh.R
@@ -0,0 +1,76 @@
+####################
+## load libraries
+####################
+library(cmdstanr) 
+library(jsonlite)
+
+#set cmdstan path
+cmdstanr::set_cmdstan_path("/work/pathfinder/cmdstan")
+
+#read in data
+dt = read_json(path="samgen3_toysim.json",simplifyVector=T)
+
+### compile stan script
+mod1     <- cmdstanr::cmdstan_model("fh_st.stan")
+
+### standardize data where necessary
+
+  M = nrow(dt)
+  K = 6          ## assumed number of clusters
+  Nst = length(unique(dt$fipsindex))
+
+  mY=mean(dt$Y)
+  msd=sqrt(mean(dt$sigma2_y))
+  y=(dt$Y-mY)/msd
+  sigma_y=sqrt(dt$sigma2_y)/msd
+  mX=mean(dt$X)
+  #x=as.matrix(cbind(rep(1,M),(dt$X-mX)/msd))
+  x=as.matrix((dt$X-mX)/msd)
+  x_var=cbind(rep(1,M),(log(dt$x_var)-mean(log(dt$x_var)))/sd(log(dt$x_var)))
+
+  oB=as.matrix(dt$B1,dt$B2)
+  moB     <- colMeans(oB)
+  MoB     <- matrix(rep(moB,times = nrow(oB)),nrow(oB),ncol(oB),byrow=TRUE)
+  soB     <- apply(oB,2,sd)
+  SoB     <- matrix(rep(soB,times = nrow(oB)),nrow(oB),ncol(oB),byrow=TRUE)
+  B   <- (oB - MoB) / SoB  ## N x Bd
+
+
+  ### prepare the list for the stan call
+  stan_data <- list( N = M,
+  	                K = K,
+		           Nst = Nst,
+		           y = y,
+		           sigma_y = sigma_y,
+		           dX = ncol(x),
+		           x = x,
+		           P = ncol(x_var),
+		           x_var = x_var,
+		           nResp = dt$nResp,
+		           Bd = ncol(B),
+		           B = B,
+                     fipsindex = dt$fipsindex
+		         )
+ 
+
+  fit1 = mod1$pathfinder(algorithm = "multi", data = stan_data,
+			   refresh = 1, num_threads = 12, num_paths = 12, psis_draws = 2000)
+
+
+
+  ##############################
+   ADVI<-function(){
+   fit2 = mod1$variational( data = stan_data,
+           	           #include = TRUE,
+	                   eta=0.1,
+	                   adapt_engaged=FALSE,
+	                   elbo_samples=1000,
+	                   output_samples=2000,
+	                   #init_r = 1.5,
+	                   tol_rel_obj = 0.0001,
+	                   iter=100000
+	                 )
+   } #### end ADVI call
+  ##############################
+
+
